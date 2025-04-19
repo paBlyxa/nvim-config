1875e471214a932ffb0aff96936d617553f3c0b4
Pavel Fakadey <pavel.fakadey@nordigy.ru>
2025-04-20 00:15:21 +0200

    Tune lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -21,6 +21,7 @@ return {
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
