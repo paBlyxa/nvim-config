8eae6b348f72b507e99bd8d4e4d181703271a184
Pavel Fakadey <pavel.fakadey@nordigy.ru>
2025-04-20 00:11:14 +0200

    Tune lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "ts_ls" }
       })
     end
   },
@@ -17,6 +17,8 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       vim.lsp.enable("lua_ls")
+      vim.lsp.enable("ts_ls")
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,16 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.diagnostic.config({
+  virtual_text = true,     -- Показывать текст диагностики рядом со строкой
+  signs = true,            -- Показывать значки в левой колонке
+  underline = true,        -- Подчеркивать проблемные места
+  update_in_insert = false, -- Не обновлять диагностику в режиме вставки
+  severity_sort = true,    -- Сортировать по серьезности
+  float = {
+    source = "always",     -- Всегда показывать источник ошибки
+    border = "rounded",    -- Скругленные рамки окна
+    header = "",           
+    prefix = "",
+  },
+})
