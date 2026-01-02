513aa3f3184231fa78783c0c21bfd71afb37aa43
Pavel Fakadey <pavel.fakadey@nordigy.ru>
2026-01-02 13:49:23 +0100

    update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,13 @@
 return {
   {
     'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', function()
         builtin.live_grep({
           layout_config = {
@@ -12,7 +15,10 @@ return {
             preview_width = 0.5
         }
       })
-      end, {})
+      end, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>fc', builtin.grep_string, { desc = 'Telescope grep string under cursor' })
     end
   },
   {
