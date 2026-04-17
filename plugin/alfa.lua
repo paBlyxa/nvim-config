9cfcadc15bc3f5aabc23bf0bcfaceea2d299fd37
Pavel Fakadey <pavel.fakadey@nordigy.ru>
2026-04-17 17:24:55 +0200

    fix greeting

--- plugin/alfa.lua
+++ plugin/alfa.lua
@@ -1,9 +1,5 @@
-vim.pack.add({ "https://github.com/echasnovski/mini.icons" })
-
-vim.pack.add({ "https://github.com/goolord/alpha-nvim" })
-
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		require("alpha").setup(require("alpha.themes.startify").config)
-	end,
+vim.pack.add({
+	{ src = "https://github.com/echasnovski/mini.icons" },
+	{ src = "https://github.com/goolord/alpha-nvim" },
 })
+require("alpha").setup(require("alpha.themes.startify").config)
